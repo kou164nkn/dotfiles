158e8686e3f9e1e763ec4653120dbc9737c1c103
Hiroshi Nakano <kou164nkn@gmail.com>
2025-12-18 00:24:11 +0900

    fixed setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -19,6 +19,8 @@ function install_package() {
 
   brew doctor
   brew bundle --verbose --file "$dotdir/.Brewfile"
+
+  command echo "Package installed"
 }
 
 function setup_vscode() {
@@ -30,19 +32,21 @@ function setup_vscode() {
     code --install-extension $EXTENSION
   done
 
-  local src_list=$(find . -mindepth 3 -type f | grep -v .git | grep Code | sed -e 's/.\///')
+  if [ -d "$dotdir/Code/User" ]; then
+    find "$dotdir/Code/User" -type f | while read -r src_file; do
+      local filename=$(basename "$src_file")
+      command ln -snf "$src_file" "$target_dir/$filename"
+    done
+  fi
 
-  for src in $src_list; do
-    src_fullpath="$dotdir/$src"
-    command ln -snf "$src_fullpath" "$target_dir/$(basename $src)"
-  done
+  command echo "VSCode settings updated"
 }
 
 function link_to_homedir() {
   command echo "backup old dotfiles..."
-  if [ ! -d "$HOME/.gdotbackup" ];then
-    command echo "$HOME/.gdotbackup not found. Auto Make it"
-    command mkdir "$HOME/.gdotbackup"
+  if [ ! -d "$HOME/.dotbackup" ];then
+    command echo "$HOME/.dotbackup not found. Auto Make it"
+    command mkdir "$HOME/.dotbackup"
   fi
 
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
@@ -54,17 +58,18 @@ function link_to_homedir() {
         command rm -f "$HOME/`basename $f`"
       fi
       if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.gdotbackup"
+        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
       fi
       command ln -snf $f $HOME
     done
+    command echo "Symbolic link created"
   else
-    command echo "same install src dest"
+    command echo "Skipped: Source and destination are the same ($HOME)."
   fi
 }
 
 function main() {
-  IS_INSTALL="true"
+  IS_INSTALL="false"
 
   while [ $# -gt 0 ];do
     case ${1} in
@@ -92,7 +97,7 @@ function main() {
     link_to_homedir
     command echo ""
     command echo "#####################################################"
-    command echo "install success!!!"
+    command echo "Setup Completed Successfully!!!"
     command echo "#####################################################"
     command echo ""
   fi
